1806aef29a31bf85feabedf602fc98b9783d8103
habadiotakis1 <habadiotakis@gmail.com>
2025-04-04 17:34:39 -0400

    a

--- app.py
+++ app.py
@@ -315,7 +315,7 @@ app_ui = ui.page_fluid(
         ),
     
     
-    ui.h5("Step 2: Select Columns"),
+    ui.h5("Step 2: Select Variables"),
     ui.output_ui('select_columns'),
     
     ui.h5("Step 3: Table Options"),
@@ -414,17 +414,6 @@ def server(input, output, session):
             for col in columns:
                 column_dict[col] = col
             
-            default_type = "Omit"
-            default_position = 15
-            
-            # Store variable settings in a dictionary
-            if not var_config.get():
-                var_config.set({col: {
-                    "type": default_type, 
-                    "name": col, 
-                    "position": default_position,
-                } for col in columns})
-
             ui.update_selectize(  
                 "column_selectize",  
                 choices={"":column_dict}
@@ -433,9 +422,11 @@ def server(input, output, session):
     @reactive.effect
     def column_selectize():
         available_columns = set(input.column_selectize())
-
         selected_columns.set(available_columns)
 
+        default_type = "Omit"
+        default_position = 15
+            
         all_subheading_values = set()
         for subheading in subheadings:
             all_subheading_values = all_subheading_values.union(set(subheadings[subheading]()))
@@ -444,6 +435,20 @@ def server(input, output, session):
             if col not in all_subheading_values:
                 subheadings["subheading_1"].set(subheadings["subheading_1"]() + [col])
         
+            # Store variable settings in a dictionary
+            if col not in var_config.get().values():
+                var_config.set({col: {
+                    "type": default_type, 
+                    "name": col, 
+                    "position": default_position,
+                } for col in available_columns})
+            # if not var_config.get():
+            #     var_config.set({col: {
+            #         "type": default_type, 
+            #         "name": col, 
+            #         "position": default_position,
+            #     } for col in available_columns})
+
         @reactive.effect
         def sync_column_selection_with_subheadings():
             for subheading in subheadings:
