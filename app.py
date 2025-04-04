a0a96f25c0ca5ba008eee7425788db709c8fb188
habadiotakis1 <habadiotakis@gmail.com>
2025-04-04 13:22:14 -0400

    a

--- app.py
+++ app.py
@@ -640,11 +640,11 @@ def server(input, output, session):
         
         updated_config = var_config.get().copy()
         
-        all_subheading_values = set()
-        for subheading in subheadings:
-            all_subheading_values = all_subheading_values.union(set(subheadings[subheading]()))
-
-        for col in all_subheading_values:
+        # all_subheading_values = set()
+        # for subheading in subheadings:
+        #     all_subheading_values = all_subheading_values.union(set(subheadings[subheading]()))
+            
+        for col in df.columns:
             print("Updating variable configurations...", updated_config[col])
             updated_config[col]["type"] = input[f"var_type_{col}"]() or "Omit"
             updated_config[col]["name"] = input[f"name_{col}"]() or col
