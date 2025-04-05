d6d9281202d4a9ba80b06b80ba420c89c866986e
habadiotakis1 <habadiotakis@gmail.com>
2025-04-05 19:45:45 -0400

    a

--- app.py
+++ app.py
@@ -345,8 +345,8 @@ app_ui = ui.page_fluid(
         ui.card(ui.input_numeric("decimals_table", "Table - # Decimals", 2, min=0, max=5)),
         ui.card(ui.input_numeric("decimals_pvalue", "P-Val - # Decimals", 3, min=0, max=5)),
         ui.card(ui.input_radio_buttons("output_format", "Output Format", ["n (%)", "% (n)"])),
-        ui.card(ui.input_radio_buttons("remove_blanks", "Remove Unknown Values (e.g., NA, Unknown)", ["No (Default)", "Yes"])),
-        # ui.card(ui.tooltip(ui.icon("info-circle"),"Customize how each variable appears in the final table.")),
+        ui.card(ui.input_radio_buttons("remove_blanks", "Remove Unknown Values", ["No (Default)", "Yes"])),
+        ui.card(ui.tooltip(ui.icon("info-circle"),"Customize how each variable appears in the final table.")),
         col_widths= (2,2,2,4,2)
         ),
 
