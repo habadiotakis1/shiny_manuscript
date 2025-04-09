90ee8d447d9ec7ce2b9775a4c688201c95656a13
habadiotakis1 <habadiotakis@gmail.com>
2025-04-09 11:13:21 -0400

    jkl

--- app_testing.py
+++ app_testing.py
@@ -349,6 +349,14 @@ app_ui = ui.page_fluid(
         ),
 
     ui.h5("Step 4: Customize Table & Rows"),
+    ui.p("Update variable names, type, subheading and position. Variables that have the same position value will be ordered alphabetically. Variable types, associated statistical test, and examples include:" \
+    "Cateogorical (Y/N): Fisher's Exact Test, examples include: Smoking, Diabetes, Hypertension " \
+    "Categorical (Dichotomous): Fisher's Exact Test, examples include: Sex" \
+    "Categorical (Multinomial): Chi2, examples include: Race" \
+    "Ratio Continuos: T-Test, examples include: Age, GF " \
+    "Ordinal Discrete: Wilcoxon, examples include: GCS, Tumor Grade" \
+    ""),
+
     # Subheadings
     ui.input_text("subheading_1", "Subheading 1", placeholder="Enter subheading 1 name"), 
     ui.output_ui("var_settings_1"),
